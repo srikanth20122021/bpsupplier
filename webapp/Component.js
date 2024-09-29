24f042302d990771c9c6cc38203d1d8d8a42dfb8
M Srikanth <srikanth20122021@gmail.com>
2024-09-29 17:56:27 +0000

    bp supplier commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.bp.bpsupplier.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
